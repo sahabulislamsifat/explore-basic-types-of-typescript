dc74ace1b6a98db00508cc84f9409b28d817079f
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-05-01 02:55:59 +0600

    type of data types

---
+++ modules/dist/1.4.js
@@ -0,0 +1,21 @@
+"use strict";
+// Data Types
+// Primitive Data
+// String
+let firstName = "Sifat";
+// Number
+let age = 22;
+// Boolean
+let isAdmin = true;
+// undefined
+let x = undefined;
+// null
+let y = null;
+// any
+// let d;
+// Array
+let friends = ["John Doe", "Elon Mask", "Ryhan Dhal"];
+let eligibleRollList = [55, 345, 64, 65];
+// eligibleRollList.push('55')
+eligibleRollList.push(45);
+console.log(eligibleRollList);
---
+++ modules/src/1.4.ts
@@ -0,0 +1,40 @@
+// Data Types
+
+// Primitive Data
+// String
+let firstName: string = "Sifat";
+
+// Number
+let age: number = 22;
+
+// Boolean
+let isAdmin: boolean = true;
+
+// undefined
+let x: undefined = undefined;
+
+// null
+let y: null = null;
+
+// any
+// let d;
+
+// Array
+
+let friends: string[] = ["John Doe", "Elon Mask", "Ryhan Dhal"];
+
+let eligibleRollList: number[] = [55, 345, 64, 65];
+
+// eligibleRollList.push('55')
+eligibleRollList.push(45);
+
+// console.log(eligibleRollList);
+
+// Tuple is a special type of ARRAY
+// array --> order --> type of values
+
+let coordinate: [number, string] = [22, "Sifat"];
+
+let coordinateTwo: [number, string, boolean] = [22, "Sifat", true];
+
+coordinateTwo[0] = 25;
