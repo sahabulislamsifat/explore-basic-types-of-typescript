fcdef3d3d8bdc42875592c7454b6cc85cd91176e
Sahabul Islam Sifat <sahabulislamsifat@gmail.com>
2025-05-02 16:54:44 +0600

    ternary, optional chaining and nullish coalescison

---
+++ modules/src/1.10.ts
@@ -0,0 +1,46 @@
+{
+  // Ternary Operator | Optional Chaining | Nullish Coalescing Operator
+
+  const age: number = 18;
+
+  if (age >= 18) {
+    // console.log("Adult");
+  } else {
+    // console.log("Not Adult");
+  }
+
+  const isAdult = age >= 18 ? "Adult" : "Not Adult";
+  //   console.log(isAdult);
+
+  // Nullish Coalescing
+  // const isAuthenticated = null;
+  const isAuthenticated = "";
+
+  const resultOne = isAuthenticated ?? "Guest";
+  const resultTwo = isAuthenticated ? isAuthenticated : "Guest";
+  // console.log({ resultOne }, { resultTwo });
+
+  // Nullish coalescing
+  type User = {
+    name: string;
+    address: {
+      road: string;
+      city: string;
+      presentAddress: string;
+      permanentAddress: string;
+    };
+  };
+
+  const user: User = {
+    name: "Sifat",
+    address: {
+      road: "Bagan bari road",
+      city: "Rangpur",
+      presentAddress: "Dhaka",
+      // permanentAddress: "Rangpur Division",
+    },
+  };
+  // console.log(user);
+  const permanentAddress = user?.address?.permanentAddress ?? "No Address";
+  console.log(permanentAddress);
+}
